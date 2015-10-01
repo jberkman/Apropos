de2e73ccab28bd974a7963174348f73557588b17
jacob berkman <jacob@87k.net>
2015-09-30 20:09:48 -0700

    Initial Commit

---
+++ Apropos/Apropos.h
@@ -0,0 +1,19 @@
+//
+//  Apropos.h
+//  Apropos
+//
+//  Created by jacob berkman on 2015-09-30.
+//  Copyright © 2015 jacob berkman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Apropos.
+FOUNDATION_EXPORT double AproposVersionNumber;
+
+//! Project version string for Apropos.
+FOUNDATION_EXPORT const unsigned char AproposVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Apropos/PublicHeader.h>
+
+
